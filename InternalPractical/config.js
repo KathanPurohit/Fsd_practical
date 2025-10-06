99bb8f497ece22ac079985fafe158eea13222582
Kathan Purohit <163106847+KathanPurohit@users.noreply.github.com>
2025-10-06 12:59:48 +0530

    Add files via upload
    Internal Practical

---
+++ InternalPractical/config.js
@@ -0,0 +1,5 @@
+// Place your OMDb API key here
+const OMDb_API_KEY = '309324da'; // <-- PASTE YOUR ACTIVATED KEY HERE
+
+// Assign the API key to window object for global access
+window.OMDb_API_KEY = OMDb_API_KEY;
